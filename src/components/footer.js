0bdb2b1bc628592e5b1a20b32ac972d897c7c075
Cleggatron <david.robert.clegg@gmail.com>
2021-10-31 10:06:38 +0000

    wrote footer

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer(){
+    return (
+        <div>
+            <a href="https://github.com/Cleggatron">Github</a>
+            <a href="https://www.linkedin.com/in/david-clegg-86b239a6/">LinkedIn</a>
+            <a href="https://stackexchange.com/users/21019404/cleggatron">Stack Overflow</a>
+        </div>
+    )
+}
+
+export default Footer;
