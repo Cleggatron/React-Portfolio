1ab9f2ac8397e3e60bf2bbbfea94176e1f5cbc71
Cleggatron <david.robert.clegg@gmail.com>
2021-10-31 12:32:12 +0000

    added img alts

--- src/components/sectionComponents/Portfolio.js
+++ src/components/sectionComponents/Portfolio.js
@@ -14,7 +14,7 @@ function Portfolio(){
                 <h3>
                     Five Star Local
                 </h3>
-                <a href="https://vast-journey-10843.herokuapp.com/login"><img src={FiveStarLocal}/></a>
+                <a href="https://vast-journey-10843.herokuapp.com/login" alt="Screenshot of the working page"><img src={FiveStarLocal}/></a>
                 <p>Technologies Used: Node, Express, MySQL, Sequelize, Handlebars, Bootstrap</p>
                 <p><a href="https://github.com/Cleggatron/My-5-Star-Local">Github Repo</a></p>
             </div>
@@ -22,7 +22,7 @@ function Portfolio(){
                 <h3>
                     Clear Sky
                 </h3>
-                <a href="https://cleggatron.github.io/ClearSky/"><img src={ClearSky}/></a>
+                <a href="https://cleggatron.github.io/ClearSky/" alt="Screenshot of the working page"><img src={ClearSky}/></a>
                 <p>Technologies Used: HTML, CSS, Javascript, Semantic UI</p>
                 <p><a href="https://github.com/Cleggatron/ClearSky">Github Repo</a></p>
             </div>
@@ -30,7 +30,7 @@ function Portfolio(){
                 <h3>
                     Employee Tracker
                 </h3>
-                <a href="https://github.com/Cleggatron/Employee-Tracker"><img src={EmployeeTracker}/></a>
+                <a href="https://github.com/Cleggatron/Employee-Tracker" alt="Screenshot of the working page"><img src={EmployeeTracker}/></a>
                 <p>Technologies Used: Node, MYSQL, Inquirer.js</p>
                 <p><a href="https://github.com/Cleggatron/Employee-Tracker">Github Repo</a></p>
             </div>
@@ -38,7 +38,7 @@ function Portfolio(){
                 <h3>
                     Note Taker
                 </h3>
-                <a href="https://vast-everglades-18367.herokuapp.com/"><img src={NoteTaker}/></a>
+                <a href="https://vast-everglades-18367.herokuapp.com/" alt="Screenshot of the working page"><img src={NoteTaker}/></a>
                 <p>Technologies Used: Node, Express, uuid.js</p>
                 <p><a href="https://github.com/Cleggatron/Note_Taker">Github Repo</a></p>
             </div>
@@ -46,7 +46,7 @@ function Portfolio(){
                 <h3>
                     Random Password Generator
                 </h3>
-                <a href="https://cleggatron.github.io/RandomPasswordGenerator/"><img src={PasswordGenerator}/></a>
+                <a href="https://cleggatron.github.io/RandomPasswordGenerator/" alt="Screenshot of the working page"><img src={PasswordGenerator}/></a>
                 <p>Technologies Used: HTML, CSS, Javascript</p>
                 <p><a href="https://github.com/Cleggatron/RandomPasswordGenerator">Github Repo</a></p>
             </div>
@@ -54,7 +54,7 @@ function Portfolio(){
                 <h3>
                     Team Profile Generator
                 </h3>
-                <a href="https://github.com/Cleggatron/TeamProfileGenerator"><img src={TeamProfileGenerator}/></a>
+                <a href="https://github.com/Cleggatron/TeamProfileGenerator" alt="Screenshot of the working page"><img src={TeamProfileGenerator}/></a>
                 <p>Technologies Used: Node, Inquirer.js, Jest.js</p>
                 <p><a href="https://github.com/Cleggatron/TeamProfileGenerator">Github Repo</a></p>
             </div>
