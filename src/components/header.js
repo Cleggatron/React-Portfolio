98c8a5821e9bcd55f853546f23d3ff541e2203e7
Cleggatron <david.robert.clegg@gmail.com>
2021-10-31 09:51:54 +0000

    wrote basic header

---
+++ src/components/header.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Header(){
+    return (
+        <div>
+            <div>
+                <h1>
+                    David Clegg
+                </h1>
+            </div>
+            <div>
+                <a href="#AboutMe">
+                    About Me
+                </a>
+                <a href="#Portfolio">
+                    Portfolio
+                </a>
+                <a href="#Contact">
+                    Contact
+                </a>
+                <a href="">
+                    CV
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
