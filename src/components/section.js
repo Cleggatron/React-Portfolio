a992a065a3ce881ad7ce8b4045d3eb03b8dc80be
Cleggatron <david.robert.clegg@gmail.com>
2021-10-31 12:30:03 +0000

    added import and tested

--- src/components/section.js
+++ src/components/section.js
@@ -2,10 +2,11 @@ import React from "react";
 import Portfolio from "./sectionComponents/Portfolio";
 import AboutMe from "./sectionComponents/AboutMe";
 import Contact from "./sectionComponents/Contact";
+import CV from "./sectionComponents/CV";
 
 function Section () {
     return(
-        <Contact />
+        <CV/>
     )
 }
 
