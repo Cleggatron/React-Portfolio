a72ecf630cfe463c94db12589638ea72e6278d27
Cleggatron <david.robert.clegg@gmail.com>
2021-10-31 10:48:43 +0000

    wrote the basic about me

--- src/components/sectionComponents/AboutMe.js
+++ src/components/sectionComponents/AboutMe.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Me from "../../assets/me.jpg"
 
 function AboutMe(){
-    
+    return (
+    <div>
+        <div>
+            <img src={Me} alt="A picture of me" />
+        </div>
+        <div>
+            <h2>
+                About Me:
+            </h2>
+            <p>
+            Full stack web developer with a background in resource planning. Lending itself to forward thinking and workload balancing. Currently completing a full stack web development bootcamp through the University of Birmingham, gaining skills in Javascript, MYSQL, and CSS. I am someone with a passion for learning new skills and applying them to solve new problems.
+            </p>
+        </div>
+    </div>
+    )
 }
 
 export default AboutMe;
