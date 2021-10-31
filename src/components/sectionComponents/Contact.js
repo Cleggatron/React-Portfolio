d14aadb7e95c977ae13813a561da6b4b0d82c626
Cleggatron <david.robert.clegg@gmail.com>
2021-10-31 12:14:49 +0000

    added basics of contact form

--- src/components/sectionComponents/Contact.js
+++ src/components/sectionComponents/Contact.js
@@ -1,7 +1,41 @@
 import React from "react"
 
 function Contact(){
+    return(
+        <div>
+            <h2> Contact Me:</h2>
+            <form>
+                <label>Name:</label>:
+                <br/>
+                <input
+                type="text"
+                placeholder="Your name here"
+                name="nameText"
+                >
+                </input>
+                <br/>
+                
+                <label>Email address:</label>
+                <br/>
+                <input
+                type="text"
+                placeholder="Your email address here"
+                name="emailText"
+                >
+                </input>
+                <br/>
 
+                <label>Message: </label>
+                <br/>
+                <textarea
+                type="text"
+                placeholder="Your message here"
+                name="messageText"
+                />
+                
+            </form>
+        </div>
+    )
 }
 
 export default Contact;
