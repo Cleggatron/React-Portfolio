324b0decb43ccd1f4b665c19b8f22f35ba0a1a81
Cleggatron <david.robert.clegg@gmail.com>
2021-10-31 20:37:02 +0000

    wrote about me css

--- src/components/projectComponents/AboutMe.js
+++ src/components/projectComponents/AboutMe.js
@@ -3,19 +3,19 @@ import Me from "../../assets/me.jpg"
 
 function AboutMe(){
     return (
-    <div>
-        <div>
-            <img src={Me} alt="A picture of me" />
+        <div className="width100 flex-vertical alignCenter justifyCenter">
+            <div className="width50">
+                <img src={Me} alt="A picture of me" className="borderRad25"/>
+            </div>
+            <div className="width50">
+                <h2>
+                    About Me:
+                </h2>
+                <p>
+                I am a junior full stack web developer with a background in Resource Planning looking to bring my problem solving skills to the table. I am currently completing a full stack web development bootcamp hosted through the University of Birmingham. I have knowledge and experience of the MERN stack as well as other technolgies MYSQL and the Jest testing library.  
+                </p>
+            </div>
         </div>
-        <div>
-            <h2>
-                About Me:
-            </h2>
-            <p>
-            I am a junior full stack web developer with a background in Resource Planning looking to bring my problem solving skills to the table. I am currently completing a full stack web development bootcamp hosted through the University of Birmingham. I have knowledge and experience of the MERN stack as well as other technolgies MYSQL and the Jest testing library.  
-            </p>
-        </div>
-    </div>
     )
 }
 
