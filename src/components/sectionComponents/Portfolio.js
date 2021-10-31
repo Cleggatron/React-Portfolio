a4a5dfe9ead77cebf3e8361ed08c78ae9965fd09
Cleggatron <david.robert.clegg@gmail.com>
2021-10-31 11:41:48 +0000

    wrote basics html for portfolio

--- src/components/sectionComponents/Portfolio.js
+++ src/components/sectionComponents/Portfolio.js
@@ -1,7 +1,65 @@
 import React from "react";
+import FiveStarLocal from "../../assets/FiveStarLocal.png"
+import ClearSky from "../../assets/ClearSky.png"
+import EmployeeTracker from "../../assets/EmployeeTracker.png"
+import NoteTaker from "../../assets/NoteTaker.png"
+import PasswordGenerator from "../../assets/PasswordGenerator.png"
+import TeamProfileGenerator from "../../assets/TeamProfileGenerator.png"
+
 
 function Portfolio(){
-
+    return(
+        <div>
+            <div>
+                <h3>
+                    Name
+                </h3>
+                <a href=""><img src={}/></a>
+                <p>Technologies Used: </p>
+                <p><a href="">Github Repo</a></p>
+            </div>
+            <div>
+                <h3>
+                    Name
+                </h3>
+                <a href=""><img src={}/></a>
+                <p>Technologies Used: </p>
+                <p><a href="">Github Repo</a></p>
+            </div>
+            <div>
+                <h3>
+                    Name
+                </h3>
+                <a href=""><img src={}/></a>
+                <p>Technologies Used: </p>
+                <p><a href="">Github Repo</a></p>
+            </div>
+            <div>
+                <h3>
+                    Name
+                </h3>
+                <a href=""><img src={}/></a>
+                <p>Technologies Used: </p>
+                <p><a href="">Github Repo</a></p>
+            </div>
+            <div>
+                <h3>
+                    Name
+                </h3>
+                <a href=""><img src={}/></a>
+                <p>Technologies Used: </p>
+                <p><a href="">Github Repo</a></p>
+            </div>
+            <div>
+                <h3>
+                    Name
+                </h3>
+                <a href=""><img src={}/></a>
+                <p>Technologies Used: </p>
+                <p><a href="">Github Repo</a></p>
+            </div>
+        </div>
+    )
 }
 
 export default Portfolio;
