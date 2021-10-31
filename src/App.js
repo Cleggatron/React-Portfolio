61824aa973bfddbbbac6017768b42989a73c5ef7
Cleggatron <david.robert.clegg@gmail.com>
2021-10-31 17:01:52 +0000

    added conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import React, { useState } from "react";
 
 import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Section from "./components/Section"
+import AboutMe from "./components/sectionComponents/AboutMe";
+import CV from "./components/sectionComponents/CV";
+import Portfolio from "./components/sectionComponents/Portfolio";
+import Contact from "./components/sectionComponents/Contact";
 
 function App() {
+
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  const renderPage = () => {
+    if (currentPage === "Home"){
+      return <AboutMe/>
+    } else if(currentPage === "CV") {
+      return <CV/>
+    } else if (currentPage === "Portfolio"){
+      return <Portfolio/>
+    } else {
+      return <Contact/>
+    }
+  }
+  
+  const handlePageChange = page => setCurrentPage(page)
+
   return (
     <div className="App">
-      <Header />
-      <Section />
+      <Header currentPage={currentPage} handlePageChange={handlePageChange}/>
+      
+      {renderPage()}
+
       <Footer />
     </div>
   );
