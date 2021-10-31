8bbb1e2e3896dfeba67ed3b27754f6f38b0240d6
Cleggatron <david.robert.clegg@gmail.com>
2021-10-31 12:15:00 +0000

    added imports

--- src/components/section.js
+++ src/components/section.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import Portfolio from "./sectionComponents/Portfolio";
+import AboutMe from "./sectionComponents/AboutMe";
+import Contact from "./sectionComponents/Contact";
 
 function Section () {
     return(
-        
+        <Contact />
     )
 }
 
