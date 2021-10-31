2be55b015a7f5c19c2e1ea4fceb484d87d9d2f7d
Cleggatron <david.robert.clegg@gmail.com>
2021-10-31 11:52:02 +0000

    added content to porfolio page

--- src/components/sectionComponents/Portfolio.js
+++ src/components/sectionComponents/Portfolio.js
@@ -12,51 +12,51 @@ function Portfolio(){
         <div>
             <div>
                 <h3>
-                    Name
+                    Five Star Local
                 </h3>
-                <a href=""><img src={}/></a>
-                <p>Technologies Used: </p>
-                <p><a href="">Github Repo</a></p>
+                <a href="https://vast-journey-10843.herokuapp.com/login"><img src={FiveStarLocal}/></a>
+                <p>Technologies Used: Node, Express, MySQL, Sequelize, Handlebars, Bootstrap</p>
+                <p><a href="https://github.com/Cleggatron/My-5-Star-Local">Github Repo</a></p>
             </div>
             <div>
                 <h3>
-                    Name
+                    Clear Sky
                 </h3>
-                <a href=""><img src={}/></a>
-                <p>Technologies Used: </p>
-                <p><a href="">Github Repo</a></p>
+                <a href="https://cleggatron.github.io/ClearSky/"><img src={ClearSky}/></a>
+                <p>Technologies Used: HTML, CSS, Javascript, Semantic UI</p>
+                <p><a href="https://github.com/Cleggatron/ClearSky">Github Repo</a></p>
             </div>
             <div>
                 <h3>
-                    Name
+                    Employee Tracker
                 </h3>
-                <a href=""><img src={}/></a>
-                <p>Technologies Used: </p>
-                <p><a href="">Github Repo</a></p>
+                <a href="https://github.com/Cleggatron/Employee-Tracker"><img src={EmployeeTracker}/></a>
+                <p>Technologies Used: Node, MYSQL, Inquirer.js</p>
+                <p><a href="https://github.com/Cleggatron/Employee-Tracker">Github Repo</a></p>
             </div>
             <div>
                 <h3>
-                    Name
+                    Note Taker
                 </h3>
-                <a href=""><img src={}/></a>
-                <p>Technologies Used: </p>
-                <p><a href="">Github Repo</a></p>
+                <a href="https://vast-everglades-18367.herokuapp.com/"><img src={NoteTaker}/></a>
+                <p>Technologies Used: Node, Express, uuid.js</p>
+                <p><a href="https://github.com/Cleggatron/Note_Taker">Github Repo</a></p>
             </div>
             <div>
                 <h3>
-                    Name
+                    Random Password Generator
                 </h3>
-                <a href=""><img src={}/></a>
-                <p>Technologies Used: </p>
-                <p><a href="">Github Repo</a></p>
+                <a href="https://cleggatron.github.io/RandomPasswordGenerator/"><img src={PasswordGenerator}/></a>
+                <p>Technologies Used: HTML, CSS, Javascript</p>
+                <p><a href="https://github.com/Cleggatron/RandomPasswordGenerator">Github Repo</a></p>
             </div>
             <div>
                 <h3>
-                    Name
+                    Team Profile Generator
                 </h3>
-                <a href=""><img src={}/></a>
-                <p>Technologies Used: </p>
-                <p><a href="">Github Repo</a></p>
+                <a href="https://github.com/Cleggatron/TeamProfileGenerator"><img src={TeamProfileGenerator}/></a>
+                <p>Technologies Used: Node, Inquirer.js, Jest.js</p>
+                <p><a href="https://github.com/Cleggatron/TeamProfileGenerator">Github Repo</a></p>
             </div>
         </div>
     )
