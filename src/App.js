2cddc24b188b4c6435f49dd9702b0fadd70e80e0
Cleggatron <david.robert.clegg@gmail.com>
2021-10-31 11:54:41 +0000

    added section import

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Section from "./components/Section"
 
 function App() {
   return (
     <div className="App">
       <Header />
-
+      <Section />
       <Footer />
     </div>
   );
