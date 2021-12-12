2a40cffb066c2b0af33cd7b35509d95640045522
Cleggatron <david.robert.clegg@gmail.com>
2021-12-12 10:04:45 +0000

    added new screenshots

--- src/components/projectComponents/Portfolio.js
+++ src/components/projectComponents/Portfolio.js
@@ -1,20 +1,26 @@
 import React from "react";
 import FiveStarLocal from "../../assets/FiveStarLocal.PNG"
 import ClearSky from "../../assets/ClearSky.PNG"
-import EmployeeTracker from "../../assets/EmployeeTracker.png"
-import NoteTaker from "../../assets/NoteTaker.PNG"
-import PasswordGenerator from "../../assets/PasswordGenerator.png"
-import TeamProfileGenerator from "../../assets/TeamProfileGenerator.png"
+import CodeShare from "../../assets/codeShare.PNG"
+import GoogleBooks from "../../assets.GoogleBooks.PNG"
 
-
-function Portfolio(){
-    return(
+function Portfolio() {
+    return (
         <div className="padding1REM flex-horizontal flex-wrap spaceAround alignCenter lightBackground">
+            <div className="width45 wrap borderBottom">
+                <h3>
+                    Code Sare
+                </h3>
+                <a className="max-width100" href="https://mighty-ridge-50767.herokuapp.com/" ><img src={CodeShare} className="max-width100" alt="Screenshot of the working page" /></a>
+                <p>Technologies Used: Node, Express, MongoDB, Mongoose, React, GraphQL, Material UI</p>
+                <p><a href="https://github.com/Cleggatron/code-sharers">Github Repo</a></p>
+            </div>
+
             <div className="width45 wrap borderBottom">
                 <h3>
                     Five Star Local
                 </h3>
-                <a className="max-width100" href="https://vast-journey-10843.herokuapp.com/login" ><img src={FiveStarLocal} className="max-width100" alt="Screenshot of the working page"/></a>
+                <a className="max-width100" href="https://vast-journey-10843.herokuapp.com/login" ><img src={FiveStarLocal} className="max-width100" alt="Screenshot of the working page" /></a>
                 <p>Technologies Used: Node, Express, MySQL, Sequelize, Handlebars, Bootstrap</p>
                 <p><a href="https://github.com/Cleggatron/My-5-Star-Local">Github Repo</a></p>
             </div>
@@ -23,45 +29,28 @@ function Portfolio(){
                 <h3>
                     Clear Sky
                 </h3>
-                <a className="max-width100" href="https://cleggatron.github.io/ClearSky/"><img src={ClearSky} className="max-width100" alt="Screenshot of the working page"/></a>
+                <a className="max-width100" href="https://cleggatron.github.io/ClearSky/"><img src={ClearSky} className="max-width100" alt="Screenshot of the working page" /></a>
                 <p>Technologies Used: HTML, CSS, Javascript, Semantic UI</p>
                 <p><a href="https://github.com/Cleggatron/ClearSky">Github Repo</a></p>
             </div>
 
-            <div className="width45 wrap borderBottom">
-                <h3>
-                    Employee Tracker
-                </h3>
-                <a className="max-width100" href="https://github.com/Cleggatron/Employee-Tracker"><img src={EmployeeTracker} className="max-width100" alt="Screenshot of the working page"/></a>
-                <p>Technologies Used: Node, MYSQL, Inquirer.js</p>
-                <p><a href="https://github.com/Cleggatron/Employee-Tracker">Github Repo</a></p>
-            </div>
+
 
             <div className="width45 wrap borderBottom">
                 <h3>
-                    Note Taker
+                    Google Book Search
                 </h3>
-                <a className="max-width100" href="https://vast-everglades-18367.herokuapp.com/"><img src={NoteTaker} className="max-width100" alt="Screenshot of the working page"/></a>
-                <p>Technologies Used: Node, Express, uuid.js</p>
-                <p><a href="https://github.com/Cleggatron/Note_Taker">Github Repo</a></p>
+                <a className="max-width100" href="https://lit-island-86967.herokuapp.com/"><img src={GoogleBooks} className="max-width100" alt="Screenshot of the working page" /></a>
+                <p>Technologies Used: Node, Express, MongoDB, Mongoose, React, GraphQL, BootStrap</p>
+                <p><a href="https://github.com/Cleggatron/GoogleBooks-GraphQL-Refactor">Github Repo</a></p>
             </div>
 
             <div className="width45 wrap borderBottom">
-                <h3>
-                    Random Password Generator
-                </h3>
-                <a className="max-width100" href="https://cleggatron.github.io/RandomPasswordGenerator/"><img src={PasswordGenerator} className="max-width100" alt="Screenshot of the working page"/></a>
-                <p>Technologies Used: HTML, CSS, Javascript</p>
-                <p><a href="https://github.com/Cleggatron/RandomPasswordGenerator">Github Repo</a></p>
+
             </div>
 
             <div className="width45 wrap borderBottom">
-                <h3>
-                    Team Profile Generator
-                </h3>
-                <a className="max-width100" href="https://github.com/Cleggatron/TeamProfileGenerator"><img src={TeamProfileGenerator} className="max-width100" alt="Screenshot of the working page"/></a>
-                <p>Technologies Used: Node, Inquirer.js, Jest.js</p>
-                <p><a href="https://github.com/Cleggatron/TeamProfileGenerator">Github Repo</a></p>
+
             </div>
         </div>
     )
